6dec2338e5aa4a8c34c202faf67b8b3d39a39d79
SpiritGreen <79371068+SpiritGreen@users.noreply.github.com>
2023-05-23 17:08:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Hey there, buddy!')
+print()
+
+name = input("What's your name? " )
+print('Such a pretty name!')
+favourite_food = input("Well, what's your favourite food? ")
+print("Sounds tasty!")
+favourite_music = input("And what's your favourite music? ")
+print('Wow! I like your music tastes!')
+print()
+
+print(f"So your name is {name}, you favourite food is {favourite_food} and you like to listen {favourite_music}")
+print("You're a cool person!")
